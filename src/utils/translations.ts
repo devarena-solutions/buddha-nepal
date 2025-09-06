f35ae2d408955cad552b8ac9a8fae2de5dee2132
Brahmdev Pandey <pandey.dev25@gmail.com>
2025-09-06 15:44:48 +0200

    refactor: move opening hours translations to translations util

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -1788,6 +1788,21 @@ export const about_us = {
   },
 };
 
+export const timings = {
+  opening_hours: {
+    en: "OPENING HOURS",
+    se: "ÖPPETTIDER",
+  },
+  weekdays: {
+    en: "Mon - Fri 11:00 - 21:00",
+    se: "Mån - Fre 11:00 - 21:00",
+  },
+  weekends: {
+    en: "Sat - Sun 13:00 - 21:00",
+    se: "Lör - Sön 13:00 - 21:00",
+  },
+};
+
 export const messages = {
   information: {
     lunch: {
